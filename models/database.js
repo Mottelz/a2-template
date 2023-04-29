343afb00d9d29c41a7e0d4c9ab832e8ce6a4d8f8
Mottel <shachna@gmail.com>
2023-04-29 18:06:12 -0400

    Added DB

---
+++ models/database.js
@@ -0,0 +1,1 @@
+// Add the database here
--- routes/public.js
+++ routes/public.js
@@ -5,3 +5,6 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('index', {title: 'Home', body: 'This is a page.'})
 })
+
+
+module.exports = router
